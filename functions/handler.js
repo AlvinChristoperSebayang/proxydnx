754d62958d9ac0a5e8c3504b12d7af3f09804895
AlvinSebayang <schristoper22@gmail.com>
2024-06-25 13:32:40 +0700

    testing

---
+++ functions/handler.js
@@ -0,0 +1,67 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+    const { queryStringParameters } = event;
+
+    const shopName = 'dxretail';
+    const apiKey = '914b749b03fe17557930217aeb716aee';
+    const password = 'shpat_87217aeb716aee';
+    const apiVersion = '2024-04';
+    const variantID = queryStringParameters.variantID;
+
+    const encodedCredentials = Buffer.from(`${apiKey}:${password}`).toString('base64');
+    const variantUrl = `https://${shopName}.myshopify.com/admin/api/${apiVersion}/variants/${variantID}.json`;
+
+    try {
+        // Fetch pertama untuk mendapatkan inventory_item_id
+        const variantResponse = await fetch(variantUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Basic ${encodedCredentials}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const variantData = await variantResponse.json();
+        const inventoryItemId = variantData.variant.inventory_item_id;
+
+        // Tentukan locationID berdasarkan data-market
+        const location = queryStringParameters.location;
+
+        // Fetch kedua untuk mendapatkan informasi inventory
+        const inventoryUrl = `https://${shopName}.myshopify.com/admin/api/${apiVersion}/inventory_levels.json?inventory_item_ids=${inventoryItemId}&location_ids=${location}`;
+        const inventoryResponse = await fetch(inventoryUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Basic ${encodedCredentials}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const inventoryData = await inventoryResponse.json();
+
+        // Menggabungkan data variant dan inventory
+        const responseData = {
+            variant: variantData.variant,
+            inventory: inventoryData
+        };
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(responseData),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            }
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            }
+        };
+    }
+};
